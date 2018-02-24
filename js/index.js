83e205c1c1fe3ffd24994887c6341836fd9a32d5
njnisarg <NJnisarg@gmail.com>
2018-02-24 12:07:32 +0530

    Added feature images and subscribe button

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(".SubscribeModal-trigger").click(function(e){
+  e.preventDefault();
+  dataSubscribeModal = $(this).attr("data-SubscribeModal");
+  $("#" + dataSubscribeModal).css({"display":"block"});
+});
+
+$(".close-SubscribeModal, .SubscribeModal-sandbox").click(function(){
+  $(".SubscribeModal").css({"display":"none"});
+});
